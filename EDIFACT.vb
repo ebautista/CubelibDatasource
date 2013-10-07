14a2b0d458d51752e57da6f36b539768bac558ca
Edwin Bautista <edwin.avellana.bautista@gmail.com>
2013-10-07 21:49:32 +0800

    Partial Implentation of Edifact DB

---
+++ EDIFACT.vb
@@ -0,0 +1,21 @@
+﻿Partial Class EDIFACT
+End Class
+
+Namespace EDIFACTTableAdapters
+    
+    Partial Public Class DATA_NCTSTableAdapter
+        Implements ITableAdapter(Of DataTable)
+
+        Public Function GetByPK(ParamArray pk() As Object) As DataTable Implements ITableAdapter(Of DataTable).GetByPK
+            Return Me.GetDataByPK(Convert.ToInt32(pk(0)))
+        End Function
+
+        Public Function RowUpdate(row As DataRow) As Integer Implements ITableAdapter(Of DataTable).RowUpdate
+            Return Update(row)
+        End Function
+
+        Public Function TableUpdate(table As DataTable) As Integer Implements ITableAdapter(Of DataTable).TableUpdate
+            Return Update(table)
+        End Function
+    End Class
+End Namespace
